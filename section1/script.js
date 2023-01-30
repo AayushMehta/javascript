ca6cf11f8733d0e3077e2248e23420c7dac4ac1c
Aayush Mehta <aayushmehta5225@gmail.com>
2023-01-30 16:58:39 +0530

    operator precidence started

--- section1/script.js
+++ section1/script.js
@@ -14,9 +14,70 @@ console.log(firstname);
 
 */
 
+/*
+
 let javaScriptIsFun = true;
 console.log(javaScriptIsFun);
 console.log(typeof true);
 
 javaScriptIsFun = "Yes!";
-console.log(javaScriptIsFun);
+// console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+//bug in js ( it should not treat null as an object)
+
+console.log(typeof null);
+
+*/
+
+/*
+
+let age = 30;
+age = 31;
+
+var job = "programmer";
+job = "teacher";
+console.log(job);
+
+*/
+
+/*
+
+const now = 2023;
+const ageAayush = now - 2001;
+const agestifl3r = now - 2019;
+console.log(ageAayush, agestifl3r);
+
+console.log(ageAayush * 2, ageAayush / 10, 2 ** 3);
+
+const firstName = "aayush";
+const lastName = "mehta";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+console.log(ageAayush > agestifl3r);
+console.log(ageAayush >= 18);
+console. log(agestifl3r >= 18);
+
+const isFullAge = ageAayush >= 18;
+console.log(now - 1991 > now - 2018);
+
+*/
+
+const now = 2023;
+const ageAayush = now - 2001;
+const agestifl3r = now - 2019;
+console.log(now - 1991 > now - 2018);
