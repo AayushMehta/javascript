1fab8313e1f6932d3d241e3c2b961cda4625548f
Aayush Mehta <aayushmehta5225@gmail.com>
2023-01-30 15:58:15 +0530

    js fundamentals

---
+++ section1/script.js
@@ -0,0 +1,22 @@
+/* 
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("aayush");
+console.log(23);
+
+let firstname = "aayushh";
+console.log(firstname);
+// console.log(firstname);
+
+firstname = "bob";
+
+console.log(firstname);
+
+*/
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+console.log(typeof true);
+
+javaScriptIsFun = "Yes!";
+console.log(javaScriptIsFun);
